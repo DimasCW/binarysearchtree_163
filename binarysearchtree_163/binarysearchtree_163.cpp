962d22af9379c844144dfb62e40ed8ce70d6a71a
DimasCW <cdimas292@gmail.com>
2023-06-30 17:40:00 +0700

    membuat 5 case

--- binarysearchtree_163/binarysearchtree_163.cpp
+++ binarysearchtree_163/binarysearchtree_163.cpp
@@ -126,8 +126,37 @@ int main() {
 
 		switch (ch)
 		{
-		default:
+		case '1':
+		{
+			cout << "Enter a word:";
+			string word;
+			cin >> word;
+			obj.insert(word);
 			break;
+
+		}
+		case'2':
+		{
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case'3':
+		{
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case '4':
+		{
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case'5':
+			return 0;
+		default:
+		{
+			cout << "invalid option" << endl;
+			break;
+		}
 		}
 	}
 }
