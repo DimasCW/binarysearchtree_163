efd73f64dc10d9d8c450026c9a58dc34f00b760f
DimasCW <cdimas292@gmail.com>
2023-06-30 17:25:12 +0700

    membuat iterasi ptr != NULL

--- binarysearchtree_163/binarysearchtree_163.cpp
+++ binarysearchtree_163/binarysearchtree_163.cpp
@@ -69,6 +69,11 @@ public:
 			cout << "tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL) {
+			inorder(ptr->leftchild);
+			cout << ptr->info << "";
+			inorder(ptr->rightchild);
+		}
 	}
 
 
